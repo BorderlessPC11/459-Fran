525aba892ef68085e64132c1e98e4db1cea70ac2
Henrique Oliveira <henriqueflashpingo@gmail.com>
2026-03-11 17:07:32 -0300

    feat: configure capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.borderless.fran',
+  appName: 'BorderlessFran',
+  webDir: 'out',
+  ios: {
+    contentInset: 'automatic',
+    backgroundColor: '#ffffff',
+  },
+};
+
+export default config;
