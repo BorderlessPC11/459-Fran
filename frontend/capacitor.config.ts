47a38fd9f7a7a2194bbd1395cee0ef099f25bb2f
Henrique Oliveira <henriqueflashpingo@gmail.com>
2026-03-11 17:07:02 -0300

    feat: turn app on compiling moment to ios

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.borderless.fran',
+  appName: 'BorderlessFran',
+  webDir: 'out'
+};
+
+export default config;
